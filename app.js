1acfee1cd4b8b68b9cfd7b7d0fcadf17d3acf8ad
Lee <lee@kudosknowledge.com>
2013-10-18 21:16:08 +1000

    Major update, re-organising, optomising etc

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3010);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(  __dirname + '/public/favicon.ico' ));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
